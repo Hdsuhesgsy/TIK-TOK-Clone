b3f6081e4fcc60d2f10174ccae8eb9ab4d33a3fe
othmanCasa10 <Othmanalif10@gmail.com>
2025-11-07 18:50:48 +0100

    Refactor Firebase initialization and configuration

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,23 +1,18 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAdh0ktN3FWOGVkkmVaamU70twjX5JQc6I",
+  authDomain: "tiktok-clone-othman-475b7.firebaseapp.com",
+  projectId: "tiktok-clone-othman-475b7",
+  storageBucket: "tiktok-clone-othman-475b7.firebasestorage.app",
+  messagingSenderId: "777771813179",
+  appId: "1:777771813179:web:242f1248ccb0ad174dcf1e"
 };
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const firestore = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
 
-// export
-export { app, auth, firestore, storage };
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
